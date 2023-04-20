f2f21fbb8d2a61d125fbd8a1db0f6502b135748a
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2023-04-20 16:30:09 -0400

    feat: basic discord client abstraction

---
+++ src/client/index.ts
@@ -0,0 +1,24 @@
+import { API_VERSION } from '..';
+import { HTTPClient } from './http';
+import { WebSocketClient } from './ws';
+
+export class DiscordClient {
+    protected ws: WebSocketClient;
+    protected http: HTTPClient;
+
+    // TODO: Implement intents
+    // TODO: Implement presence
+    constructor(token: string) {
+        this.http = new HTTPClient(token);
+        this.ws = new WebSocketClient(token);
+    }
+
+    async connect() {
+        const { url, shards } = await this.http.getGatewayBot();
+
+        console.log(`Obtaining gateway connection to ${url} with ${shards} recommended shards`);
+
+        // TODO: Implement compression
+        await this.ws.connect(`${url}/?v=${API_VERSION}&encoding=json`);
+    }
+}
